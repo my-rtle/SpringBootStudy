77966b401e63e1b1052c6ac8afe007fc55fefa92
dnekgus <sallywoo2004@naver.com>
2025-03-26 20:26:12 +0900

    공부한거

--- SpringBootStudy/WebServerOnlyJava/src/main/java/ClientHandler.java
+++ SpringBootStudy/WebServerOnlyJava/src/main/java/ClientHandler.java
@@ -16,11 +16,37 @@ public class ClientHandler implements Runnable{
             String requestLine = in.readLine();
             System.out.println("클라이언트 요청: "+requestLine);
 
+            boolean isPost = requestLine.startsWith("POST");
+
+            int contentLength = 0;
+            String line;
+            while (!(line = in.readLine()).isEmpty()) {
+                System.out.println("헤더: " + line);
+                if (line.startsWith("Content-Length:")) {
+                    contentLength = Integer.parseInt(line.split(": ")[1]);
+                }
+            }
+
+
+            StringBuilder requestBody = new StringBuilder();
+            if (isPost && contentLength > 0) {
+                char[] bodyData = new char[contentLength];
+                in.read(bodyData);
+                requestBody.append(bodyData);
+            }
+
             String response = "HTTP/1.1 200 OK\r\n" +
                                 "Content-Type: text/html\r\n" +
-                                "Connection: close\r\n\r\n" +
-                                "<h1>Hello, Client!</h1>";
+                                "Connection: close\r\n\r\n" ;
             out.write(response);
+
+            if (isPost) {
+                out.write("<h1>POST 요청을 받았습니다!</h1>");
+                out.write("<p>데이터: " + requestBody.toString() + "</p>");
+            } else {
+                out.write("<h1>GET 요청을 받았습니다!</h1>");
+            }
+
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
--- SpringBootStudy/WebServerOnlyJava/src/main/java/HttpServer.java
+++ SpringBootStudy/WebServerOnlyJava/src/main/java/HttpServer.java
@@ -2,12 +2,14 @@ import java.io.*;
 import java.net.*;
 public class HttpServer {
     public static void main(String[] args) throws IOException{
-        int port = 8080;
+        int port = 9090;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("서버 시작! http://localhost:"+port);
 
         while (true){
+            System.out.println("클라이언트 요청 대기 중...");
             Socket clientSocket = serverSocket.accept();
+            System.out.println("클라이언트 연결됨!");
             new Thread(new ClientHandler(clientSocket)).start();
         }
 
